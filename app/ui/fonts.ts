8b9f57e13357867c83496eef386e63739a0f6c59
Sincan Maulana <sincanmaulanaa@gmail.com>
2023-12-01 14:06:15 +0700

    Initialize project

---
+++ app/ui/fonts.ts
@@ -0,0 +1,7 @@
+import { Inter, Lusitana } from 'next/font/google';
+
+export const inter = Inter({ subsets: ['latin'] });
+export const lusituna = Lusitana({
+  weight: ['400', '700'],
+  subsets: ['latin'],
+});
